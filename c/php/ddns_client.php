1e4be358e3db8693532ba6ee859d6a5c071932e5
Donghua.Lau <1485084328@qq.com>
2014-11-21 10:57:48 +0800

    update php ddns client script

--- c/php/ddns_client.php
+++ c/php/ddns_client.php
@@ -4,7 +4,6 @@ $domain = "";
 $host = "default";
 
 if($argc == 2){
-    //var_dump($argv);
     $domain = $argv[1];
 }else if($argc == 3){
     $domain = $argv[1];
@@ -13,6 +12,13 @@ if($argc == 2){
 
 $url = "http://www.becktu.com:1080/main.cgi?md=ddns&dm=".$domain."&host=".$host;
 $msg = file_get_contents($url);
-echo $msg;
+Logging($msg);
+
+function Logging($log)
+{
+    $logfile = "ddns_client.log";
+    $date = date('Y-m-d H:i:s')." - ";
+    file_put_contents($logfile, $date.$log."\n", FILE_APPEND);
+}
 
 ?>
