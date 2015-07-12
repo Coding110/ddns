3a8b7a34e519ce3929a045cc2e282eef1c488b4d
Donghua <hua_zhixing@163.com>
2015-07-13 07:36:33 +0800

    update php client

---
+++ php/c/ddns_client.php
@@ -0,0 +1,74 @@
+<?php
+
+global $g_key_word;
+$g_key_word = "";
+
+main($argc, $argv);
+function main($argc, $argv)
+{
+	$domain = "";
+	$host = get_nat_ip();
+	
+	if($argc == 2){
+	    $domain = $argv[1];
+	}else if($argc == 3){
+	    $domain = $argv[1];
+	    $host = $argv[2];
+	}
+
+	$ts = time();
+	$key = create_key($ts, $domain);
+	//echo $key."\n";
+
+	$url = "http://dyn.becktu.com:8080/ddns?md=update&dm=$domain&host=$host&ts=$ts&key=$key";
+	echo $url."\n";
+
+	$msg = file_get_contents($url);
+	echo $msg."\n";
+	Logging($msg);
+}
+
+function create_key($ts, $domain)
+{
+	global $g_key_word;
+	$str = $ts.$g_key_word.$domain;
+	$md5str = md5($str);
+	$key = $md5str;
+	return $key;
+}
+
+function main_old($argc, $argv)
+{
+	$domain = "";
+	//$host = "default";
+	$host = get_nat_ip();
+	
+	if($argc == 2){
+	    $domain = $argv[1];
+	}else if($argc == 3){
+	    $domain = $argv[1];
+	    $host = $argv[2];
+	}
+	
+	$url = "http://www.becktu.com:1080/main.cgi?md=ddns&dm=".$domain."&host=".$host;
+	$msg = file_get_contents($url);
+	Logging($msg);
+}
+
+function Logging($log)
+{
+	date_default_timezone_set("PRC");
+	$logfile = "/home/hua/bin/ddns_client.log";
+	$date = date('Y-m-d H:i:s')." - ";
+	file_put_contents($logfile, $date.$log."\n", FILE_APPEND);
+}
+
+function get_nat_ip()
+{
+	$dyn_check_ip_url = "http://opendata.baidu.com/api.php?query=ip&resource_id=6006";
+	$msg = file_get_contents($dyn_check_ip_url);
+	$host_info = json_decode($msg);
+	return $host_info->data[0]->origip;
+}
+
+?>
